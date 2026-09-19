9079131077d0202efc5d2a8f664ababf615d6786
dinaarshad439 <dinaarshad439@gmail.com>
2026-09-19 16:19:16 +0300

    Question 1 answer

---
+++ C#Basics04/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Basics04
+{
+    internal class Book
+    {
+
+        private string Password = "Secret";
+
+    }
+}
--- C#Basics04/Program.cs
+++ C#Basics04/Program.cs
@@ -4,6 +4,19 @@
     {
         static void Main(string[] args)
         {
+
+            #region (Q1) Accessing a Private Field from Main
+
+            Book book = new Book();
+            Console.WriteLine(book.Password);
+
+            /*
+             * It will result in a compilation error. 
+             * As Variable Password is private so its accessible only inside the class.
+             */
+
+            #endregion
+
             
         }
     }
